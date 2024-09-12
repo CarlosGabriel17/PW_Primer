bc9c1fe19f12936822cbb26bd5d769a4ddd4ce0c
CarlosGabriel17 <CHARLY.PUF117@GMAIL.COM>
2024-09-11 20:12:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// Seleccionamos el botón y le añadimos un evento de clic
 document.getElementById('changeColorBtn').addEventListener('click', function() {
-    // Cambia el color de fondo del cuerpo de la página
     document.body.style.backgroundColor = getRandomColor();
 });
 
-// Función para generar un color aleatorio
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
